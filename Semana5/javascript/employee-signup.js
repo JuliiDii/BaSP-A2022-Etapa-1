80c6d212fcd6a10ee11ba01b57e1add9c5338560
Julieta Diaz <juliidii12@gmail.com>
2022-09-27 10:15:29 -0300

    BaSP-Week-05-a2022 WP

---
+++ Semana5/javascript/employee-signup.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    var signUpErrors = {
+        name :'Required',
+        surname :'Required',
+        password: 'Required',
+        email: 'Required',
+        dni : 'Required',
+        dateofbirth : 'Required',
+        phone :'Required',
+        address : 'Required',
+        locality : 'Required',
+        postcode :'Required',
+        email : 'Required',
+        password : 'Required',
+    };
+    var nameInput = document.getElementById ("name");
+    var surnameInput = document.getElementById("surname");
+    var passwordInput = document.getElementById("password");
+    var emailInput = document.getElementById("email");
+    var dniInput = document.getElementById("dni");
+    var dateOfBirthInput  = document.getElementById("dateofbirth");
+    var phoneInput = document.getElementById ("phone");
+    var addressInput = document.getElementById("address");
+    var localityInput = document.getElementById("locality");
+    var postCodeInput = document.getElementById("postcode");
+    var emailInput = document.getElementById("email");
+    var passwordInput = document.getElementById("password");
+
+        
+
+}
--- Semana5/javascript/login.js
+++ Semana5/javascript/login.js
@@ -1,13 +1,72 @@
-window.onload = function(){
-    var email = document.getElementById("email");
-    function validarEmail (email){
-        var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-        if (email.value === ""){
-            email.classlist.add("redBorder");
+window.onload = function() {
+    var signInErrors = {
+        password: 'Required',
+        email: 'Required'
+    };
+    var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+    var emailInput = document.getElementById("email");
+    var passwordInput = document.getElementById("password");
+    var signInButton = document.getElementById("button");
 
-    }
-    email.onblur= function(){
-    validarEmail
+    emailInput.onblur = function() {
+        var inputValue = emailInput.value;
+        if (!emailExpression.test(inputValue)) { // if (emailExpression.test(inputValue) === false)
+            var errorMessage = "Invalid email format";
+            signInErrors.email = errorMessage;
+            emailInput.classList.add('redBorder');
+            var errorMessageElement = document.createElement('p');
+            errorMessageElement.setAttribute('id', 'emailError');
+            errorMessageElement.innerText = errorMessage;
+            emailInput.insertAdjacentElement('afterend', errorMessageElement);
+        } else {
+            signInErrors.email = null;
+            emailInput.classList.add('greenBorder');
+        }
     }
 
+    emailInput.onfocus = function() {
+        emailInput.classList.remove('greenBorder');
+        emailInput.classList.remove('redBorder');
+        var emailError = document.getElementById('emailError');
+        if (emailError) {
+            emailError.remove();
+        }
+    }
+
+    passwordInput.onblur = function() {
+        var inputValue = passwordInput.value;
+        if (inputValue === "") { // if (!inputValue)
+            var errorMessage = "Password input is required";
+            signInErrors.password = errorMessage;
+            passwordInput.classList.add('redBorder');
+            var errorMessageElement = document.createElement('p');
+            errorMessageElement.setAttribute('id', 'passwordError');
+            errorMessageElement.innerText = errorMessage;
+            passwordInput.insertAdjacentElement('afterend', errorMessageElement);
+        } else {
+            signInErrors.password = null;
+            passwordInput.classList.add('greenBorder');
+        }
+    }
+
+    passwordInput.onfocus = function() {
+        passwordInput.classList.remove('greenBorder');
+        passwordInput.classList.remove('redBorder');
+        var passwordError = document.getElementById('passwordError');
+        if (passwordError) {
+            passwordError.remove();
+        }
+    }
+
+    signInButton.onclick = function(event) {
+        event.preventDefault();
+        var errors = Object.values(signInErrors);
+        var currentErrors = errors.filter(error => error !== null); // Me va a devolver todos los errores
+        if (currentErrors.length > 0) {
+            alert('Existen errores en el formulario');
+        } else {
+            var alertMessage = "Input values: " + passwordInput.value + " " + emailInput.value;
+            alert(alertMessage);
+        }
+    }
 }
